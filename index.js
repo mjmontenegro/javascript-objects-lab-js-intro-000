340d16cbbf5a2049aee8b75a21bfc634aecb649a
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-12 21:30:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { key: value });
+  let newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return newObj;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
